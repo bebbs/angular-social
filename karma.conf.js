38f0c8aea81bf1fbd2219b475c33c818804875ce
Josh Bebbington <josh@focalwebsites.com>
2015-03-28 11:14:56 +0000

    Creates a blank angular app, with bower, and karma

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+// Karma configuration
+// Generated on Sat Mar 28 2015 11:12:15 GMT+0000 (GMT)
+
+module.exports = function(config) {
+  config.set({
+
+    basePath: 'http://localhost:8080',
+    frameworks: ['jasmine'],
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'app/**/*.js',
+      'spec/**/*.js'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false
+  });
+};
