629edb1516fbb7934560a91a3d8f40d77a6c46ea
Josh Bebbington <josh@focalwebsites.com>
2015-03-28 11:52:57 +0000

    Grunt is managing end-to-end tests with protractor

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,26 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     karma: {
       unit: {
         configFile: 'karma.conf.js'
       }
+    },
+
+    protractor: {
+      options: {
+        configFile: 'protractor.conf.js',
+        keepAlive: true,
+        noColor: false,
+        args: {}
+      },
+      all: {}
     }
   });
 
   grunt.loadNpmTasks('grunt-karma');
-  grunt.registerTask('default', ['karma']);
+  grunt.loadNpmTasks('grunt-protractor-runner');
+
+  grunt.registerTask('default', ['karma', 'protractor']);
 };
